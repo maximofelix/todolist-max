8941baca43065d8d5dcb34a957768ed22782167b
maximofelix <maximofelix@gmail.com>
2023-05-12 00:14:56 -0300

    Layouts + Add em tela
    Conclusão dos ajustes no layout.
    Adicionada a funcionalidade do botão de Adicionar tarefa

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,50 @@
-function iniciar(){
+// Seleção de elementos
+const todoListItens = JSON.parse(localStorage.getItem("todoListItens")) || [];
+const todoForm = document.getElementById('todo-form');
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+const editForm = document.getElementById("edit-form");
+const editInput = document.getElementById("edit-input");
+const cancelEditBtn = document.getElementById("cancel-edit-btn");
+
+// Funções
+const saveTodo = (texto) => {
+    const todo = document.createElement("div")
+    todo.classList.add("todo")
+
+    const todoTitle = document.createElement("h3")
+    todoTitle.innerText = texto
+    todo.appendChild(todoTitle)
     
+    const doneBtn = document.createElement('button')
+    doneBtn.classList.add("finish-todo")
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn)
+    
+    const editBtn = document.createElement('button')
+    editBtn.classList.add("edit-todo")
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn)
+    
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add("remove-todo")
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(deleteBtn)
+
+    todoList.appendChild(todo)
+    todoInput.value = ""
+    todoInput.focus()
 }
+
+// Eventos
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputValue = todoInput.value;
+    if (inputValue)
+        saveTodo(inputValue);
+});
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert('alterou');
+});
