d235b80bed30195f90b7f14f05d60320772f5b8e
maximofelix <maximofelix@gmail.com>
2023-05-14 00:39:25 -0300

    Edição e Filtro implementados
    Edição e Filtro implementados

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+let todoEditId = 0;
 // Seleção de elementos
 const todoForm = document.getElementById('todo-form');
 const todoInput = document.getElementById("todo-input");
@@ -6,6 +7,7 @@ const editForm = document.getElementById("edit-form");
 const editInput = document.getElementById("edit-input");
 const cancelEditBtn = document.getElementById("cancel-edit-btn");
 const toolbar = document.getElementById("areatoolbar");
+const searchInput = document.getElementById("search-input");
 
 // Funções
 // filtro = pessoas.find((pes) => pes.login.contains(pessoa.login));
@@ -38,23 +40,67 @@ class Tarefa {
     // Status 1:Aberta, 2:Concluida, 3:Cancelada, 5:Atrasada
 }
 
-const saveTodo = (tarefa) => {
-    
-    const task = new Tarefa(tarefa);
+const obterTodoList =() => {
     const tasks = JSON.parse(localStorage.getItem("todolistmax")) || [];
+    return tasks;
+}
+
+const saveTodoAdd = (tarefa) => {
+    const task = new Tarefa(tarefa);
+    const tasks = obterTodoList();
     tasks.push(task);
     localStorage.setItem("todolistmax", JSON.stringify(tasks))
     return task;
 }
 
+const saveTodoEdit = (task) => {
+    
+    const tasks = obterTodoList();
+    let objIndex = tasks.findIndex((obj => obj.id == todoEditId));
+    tasks[objIndex].titulo= task.titulo
+    tasks[objIndex].concluida= task.concluida
+    localStorage.setItem("todolistmax", JSON.stringify(tasks))
+    return tasks[objIndex];
+
+    // const tasks = obterTodoList();
+    // const tasksNova = tasks.filter(t => t.id != todoEditId);
+    // let taskEdit = tasks.filter(t => t.id == todoEditId);
+    // taskEdit.titulo = task.titulo;
+    // taskEdit.concluida = task.concluida;
+    // tasksNova.push(task);
+    // localStorage.setItem("todolistmax", JSON.stringify(tasksNova))
+    // return taskEdit;
+}
+
 const removeTodo = (tarefaId) => {
-    const tasks = JSON.parse(localStorage.getItem("todolistmax")) || [];
+    const tasks = obterTodoList()
     const novatasks = tasks.filter(t => (t.id != tarefaId));
     localStorage.setItem("todolistmax", JSON.stringify(novatasks))
 }
 
+const limparGrid = () => {
+    let itens = todoList.querySelectorAll('div');
+    let tam = itens.length;
+    while (tam > 0){
+        itens[tam-1].remove();
+        tam--;
+    }
+}
+
+const filtrarItens = (termo) => {
+    limparGrid();
+    const tasks = obterTodoList();
+    let filtro = tasks.filter(function (str) { return str.titulo.includes(termo); });
+
+    console.log(filtro.length)
+    filtro.forEach(task => {
+        carregarItemNaGrade(task)
+    });
+}
+
 const carregarItens = () => {
-    const tasks = JSON.parse(localStorage.getItem("todolistmax")) || [];
+    limparGrid();
+    const tasks = obterTodoList();
     tasks.forEach(task => {
         carregarItemNaGrade(task)
     });
@@ -73,7 +119,7 @@ const carregarItemNaGrade = (tarefa) => {
     todo.appendChild(todoTitle)
 
     const todoId = document.createElement('input')
-    todoId.type = 'text'
+    todoId.type = 'hidden'
     todoId.value = tarefa.id
     todoId.name = 'hid-id'
     todo.appendChild(todoId)
@@ -110,9 +156,18 @@ todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const inputValue = todoInput.value;
     if (inputValue){
-        const task = saveTodo(inputValue);
-        carregarItemNaGrade(task)
+        const task = saveTodoAdd(inputValue);
+        carregarItens()
     }
+});
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let task = new Tarefa(editInput.value)
+    task.id = todoEditId;
+    task = saveTodoEdit(task)
+    carregarItens();
+    toggleForms()
 });
 
 document.addEventListener("click", (e) => {
@@ -121,13 +176,13 @@ document.addEventListener("click", (e) => {
     let todoId;
     let todoTitle;
 
-    console.log(targetEl)
-    console.log(parentEl)
+    // console.log(targetEl)
+    // console.log(parentEl)
     if (parentEl && parentEl.querySelector('h3')){
         todoTitle = parentEl.querySelector('h3')
-        console.log(todoTitle.innerText)
+        // console.log(todoTitle.innerText)
         todoId = parentEl.querySelector('input')
-        console.log(todoId.value)
+        // console.log(todoId.value)
 
     }
 
@@ -136,11 +191,14 @@ document.addEventListener("click", (e) => {
     }
 
     if (targetEl.classList.contains('remove-todo')){
-        removeTodo(todoId)
-        parentEl.remove();
+        removeTodo(todoId.value)
+        //parentEl.remove();
+        carregarItens();
     }
 
     if (targetEl.classList.contains('edit-todo')){
+        todoEditId = todoId.value;
+        editInput.value = todoTitle.innerText;
         toggleForms();
     }
 });
@@ -149,3 +207,8 @@ cancelEditBtn.addEventListener("submit", (e) => {
     e.preventDefault();
     toggleForms()
 });
+
+toolbar.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    filtrarItens(searchInput.value)
+})
